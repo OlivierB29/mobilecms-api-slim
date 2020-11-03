0b2e5ecfcff8b787754d42c7df775b20ae9ced20
Olivier <olivier_boniteau@yahoo.fr>
2020-11-03 22:14:49 +0100

    interface

--- src/ApiConstants.php
+++ src/ApiConstants.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace App;
 
-class ApiConstants
+interface ApiConstants
 {
     public const ROOT = '/mobilecmsapi';
     public const VERSION = '/v2';
 
     public const API = self::ROOT . self::VERSION;
+
+
 }
