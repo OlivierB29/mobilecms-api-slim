900902acfe62b6b631cc73af1c37fc031cf2d987
Olivier Boniteau <OlivierB29@users.noreply.github.com>
2021-01-16 18:00:06 +0000

    Apply fixes from StyleCI

--- tests/Infrastructure/Utils/MailUtilsTest.php
+++ tests/Infrastructure/Utils/MailUtilsTest.php
@@ -14,6 +14,5 @@ final class MailUtilsTest extends TestCase
         $u = new MailUtils('');
         $result = $u->getFrom('foo@bar.org');
         $this->assertTrue(strpos($result, 'foo@bar.org') !== false);
-
     }
 }
