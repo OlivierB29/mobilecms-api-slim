1e50045dddc01bf81f4bd6e028efda6c45e55936
Olivier Boniteau <olivier_boniteau@yahoo.fr>
2024-07-04 21:13:53 +0200

    last version : new URI

--- src/ApiConstants.php
+++ src/ApiConstants.php
@@ -7,7 +7,7 @@ namespace App;
 interface ApiConstants
 {
     public const ROOT = '/mobilecmsapi';
-    public const VERSION = '/v40';
+    public const VERSION = '/v42';
 
     public const API = self::ROOT.self::VERSION;
 }
