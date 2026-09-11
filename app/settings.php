51bbcf3690634561f3dc83ebe788450d07e59d86
Olivier Boniteau <olivier_boniteau@yahoo.fr>
2026-09-11 13:40:31 +0200

    configuration rework

--- app/settings.php
+++ app/settings.php
@@ -7,7 +7,7 @@ use DI\ContainerBuilder;
 use Monolog\Logger;
 
 return function (ContainerBuilder $containerBuilder) {
-    Properties::init($_SERVER['DOCUMENT_ROOT'], $_SERVER['DOCUMENT_ROOT'].'/api/conf/conf.json');
+    Properties::init($_SERVER['DOCUMENT_ROOT'], $_SERVER['DOCUMENT_ROOT'].'../private/conf/conf.json');
 
     // Global Settings Object
     $containerBuilder->addDefinitions([
